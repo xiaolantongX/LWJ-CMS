65072597a71492a7f993a70d6c07c73232b47b7d
lwj <1494782535@qq.com>
2021-09-12 16:24:53 +0800

    fix: ok

---
+++ vue.config.js
@@ -0,0 +1,32 @@
+// const path = require('path')
+
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://152.136.185.210:5000/',
+        pathRewrite: {
+          '^/api': ''
+        },
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        views: '@/views'
+      }
+    }
+  }
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     views: '@/views'
+  //   }
+  // },
+  // chainWebpack: (config) => {
+  //   config.resolve.alias.set('@', path.resolve(__dirname, 'src')).set('views', '@/views')
+  // }
+}
